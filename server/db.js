76a218b5c114cf732dc335f59fc08a9f20cce445
Chantel Yip <chantelyip422@gmail.com>
2021-01-15 00:42:35 -0800

    added database

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "poop",
+  host: "localhost",
   port: 5432,
   database: "albums",
 });
--- server/server.js
+++ server/server.js
@@ -6,27 +6,72 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+//add images
 app.post("/albums", async (req, res) => {
   try {
     const { description } = req.body;
     const addImage = await pool.query(
-      "INSERT INTO personal(description) VALUES($1) RETURNING *",
+      "INSERT INTO personal (description) VALUES($1) RETURNING *",
       [description]
     );
 
-    res.json(addImage.rows[0]);
+    res.json(addImage.rows);
   } catch (err) {
     console.error(err.message);
   }
 });
 
-app.get("/albums/:id", async (req, rest) => {
+//get all images
+app.get("/albums", async (req, res) => {
+  try {
+    const allImages = await pool.query("SELECT * FROM personal");
+    res.json(allImages.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get an image
+
+app.get("/albums/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const image = await pool.query("SELECT * FROM todo WHERE image_id = $1", [
-      id,
-    ]);
-    res.json(image.rows[0]);
+    const personal = await pool.query(
+      "SELECT * FROM personal WHERE image_id = $1",
+      [id]
+    );
+    res.json(personal.rows[0]);
+    console.log(req.params);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//update
+app.put("/albums/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("hi");
+    const { description } = req.body;
+    const updateImage = await pool.query(
+      "UPDATE personal SET description = $1 WHERE image_id = $2",
+      [description, id]
+    );
+    res.json("Image was updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete
+app.delete("/albums/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteImage = await pool.query(
+      "DELETE FROM personal WHERE image_id = $1",
+      [id]
+    );
+    res.json("image was deleted");
   } catch (err) {
     console.log(err.message);
   }
